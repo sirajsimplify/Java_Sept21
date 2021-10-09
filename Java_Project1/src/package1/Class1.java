5f473c7703fa04b4a1b2c3e77192db8733493c57
sirajsimplify <sirajparveez@gmail.com>
2021-10-10 01:23:59 +0530

    Added todays Session

--- Java_Project1/src/package1/Class1.java
+++ Java_Project1/src/package1/Class1.java
@@ -3,8 +3,12 @@ package package1;
 public class Class1 {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Heaven");
 		System.out.println("From the new line");
+	}
+	
+	public void method1() {
+		System.out.println("786");
 	}
 
 }
