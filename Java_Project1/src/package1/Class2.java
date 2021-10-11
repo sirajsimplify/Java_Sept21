9bca1222e18348a638362b20cee9715cfa9e5388
sirajsimplify <sirajparveez@gmail.com>
2021-10-11 18:06:43 +0530

    06:06PM

--- Java_Project1/src/package1/Class2.java
+++ Java_Project1/src/package1/Class2.java
@@ -2,28 +2,19 @@ package package1;
 
 import java.sql.SQLException;
 
-public class Class2 {
-	
-	public void m1() throws Exception{
-		System.out.println("Hello");
+public class Class2 extends ClassD{
+	ClassD d;
+	Class2(ClassD d){
+		this.d = d;
+		System.out.println(d);
 	}
 	
-	public void m2() throws Exception {
-		m1();
+	public void method1() {
+		ClassD c1 = new ClassD();
+		System.out.println(c1);
 	}
 	
-	public void m3() throws Exception {
-		m2();
-	}
 	
-	public static void main(String[] args) {
-		Class2 c = new Class2();
-		try {
-			c.m3();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.out.println("End");
-	}
+	
 	
 }
--- Java_Project1/src/package1/ClassD.java
+++ Java_Project1/src/package1/ClassD.java
@@ -1,5 +1,22 @@
 package package1;
 
 public class ClassD {
+	
+	public static void main(String[] args) {
+		ClassD c = new ClassD();
+		System.out.println(c);		
+		Class2 c2 = new Class2(c);	
+		
+		c.addNumber(2, 3);  // Call by value
+		c.showValue(c);  // Call by reference
+	}
+	
+	public void addNumber(int a, int b) {
+		System.out.println(a+b);
+	}
+	
+	public void showValue(ClassD d) {
+		System.out.println(d);
+	}
 
 }
